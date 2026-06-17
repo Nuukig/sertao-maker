d078bdd77c279d59d23e40d386080083b3094c36
Nuukig <igorlucenacamboim3@gmail.com>
2026-06-17 16:12:33 -0300

    fix: db keys

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = 'https://aikjinlkzkefacjzvwjz.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFpa2ppbmxremtlZmFjanp2d2p6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE2MzYxOTYsImV4cCI6MjA5NzIxMjE5Nn0.7SHaZ46NUlvI2fxW84id26N-GrZxSn7quZcV2cHCyH4';
+const SUPABASE_URL = process.env.URL_SUPABASE;
+const SUPABASE_ANON_KEY = process.env.KEY_SUPABASE;
 
 const GOOGLE_DRIVE_FOLDER_ID = 'ID_DA_PASTA_NO_DRIVE';
 const GOOGLE_API_KEY = 'SUA_GOOGLE_API_KEY';
